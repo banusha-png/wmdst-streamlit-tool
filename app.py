edf8e558cd09423d739f06a8f420aed22849d838
banusha-png <arudchelvanbanusha@gmail.com>
2026-05-17 22:05:20 +0530

    Remove page icon from Streamlit config
    Remove page icon from Streamlit page configuration.

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import streamlit as st
 
 st.set_page_config(
     page_title="WMDST | Waste Mapping Decision Support Tool",
-    page_icon="📊",
     layout="wide",
     initial_sidebar_state="expanded"
 )
