dd02ee72aac3cb522c2007b7b0bd2fbc4e717b9b
banusha-png <arudchelvanbanusha@gmail.com>
2026-05-17 23:43:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1383,8 +1383,9 @@ elif page == "8. User Guide":
 
     st.markdown(
         """
-        **Step 1 - Enter Production data**
-        *Input the required process data for each department:*
+        ###Step 1 - Enter Production data
+        <br>
+        <i>Input the required process data for each department:</i>
         - Department name
         - Cycle time
         - Throughput time
