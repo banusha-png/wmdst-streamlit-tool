acb429b029f1882c8e02afae91d0f53e292b8e6d
Banusha Arudchelvan <arudchelvanbanusha@gmail.com>
2026-05-19 14:54:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1422,6 +1422,8 @@ elif page == "8. User Guide":
         - Rate waste relationship strength on a scale of 0 (No Relationship) to 3 (Strong relationship)
 
         📌 *Sample Questions (Adjust to Suit Your Factory Procedures)*
+
+        📝 Collect responses using this questionnaire from relevant personnel across different departments to ensure diverse operational perspectives and improve the reliability of the assessment results.
         
         **Waiting:** 1) Delays occur between one process and the next; 2) Operators or machines remain idle due to material or information delays; 3) Approval, Inspection or machine setup delays affect production flow.
         
